3631fd7b73c0cbf68fcdd0a6f123b408be40d01e
Leon Friedmann <leon@friedmann.eu>
2022-04-27 10:42:32 +0200

    Outsourced static functions to utils.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from BeliefBase import BeliefBase
+from utils import DPLL
 
 class MyTestCase(unittest.TestCase):
     def test_telling_sentence_behaviour(self):
@@ -16,6 +17,21 @@ class MyTestCase(unittest.TestCase):
 
         bb.tell(clause_2)
         self.assertEqual(len(bb.get_knowledge_base()), 3)
+
+    def test_dpll(self):
+        input_sentence_false = "(~n | ~t) & (m | q | n) & (l | ~m) & (l | ~q) & (~l | ~p) & (r | p | n) & (~r | ~l) & (t)"
+
+        bb_false = BeliefBase()
+
+        bb_false.tell(input_sentence_false)
+        self.assertFalse(DPLL(bb_false.get_knowledge_base()))
+
+        input_sentence_true = "(p | q | r) & (p | q | ~r) & (p | ~q | r) & (p | ~q | ~r) & (~p | q | r) & (~p | q | ~r) & (~p | ~q | r)"
+
+        bb_true = BeliefBase()
+        bb_true.tell(input_sentence_true)
+
+        self.assertTrue(DPLL(bb_true.get_knowledge_base()))
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,85 @@
+import math
+from sympy import to_cnf
+from typing import List, Union
+from clause import Clause
+
+
+def DPLL(kb: List):
+    unit_clauses = get_unit_clauses(kb)
+    while unit_clauses:
+        for clause in unit_clauses:
+            kb = simplify(kb, clause)
+
+            if kb == False:  # contradiction was found
+                return False
+            elif len(kb) == 0:  # all terms were removed
+                return True
+
+        unit_clauses = get_unit_clauses(kb)
+
+    shortest_clause = ""
+    shortest_clause_terms = math.inf
+
+    for clause in kb:
+        terms = clause.count('|')
+        if terms < shortest_clause_terms:
+            shortest_clause = clause
+            shortest_clause_terms = terms
+
+    term = shortest_clause.split(' |')[0]
+
+    if DPLL([term] + kb):
+        return True
+    else:
+        neg_term = str(to_cnf(f"~({term})"))
+        return DPLL([neg_term] + kb)
+
+
+def get_unit_clauses(kb: List) -> List:
+    unit_clauses = []
+    for clause in kb:
+        # if unit clause
+        if '|' not in clause:
+            unit_clauses.append(clause)
+
+    return unit_clauses
+
+
+def simplify(kb: List, unit: str) -> Union[List, bool]: # TODO: Why return bool here?
+    neg_unit = str(to_cnf(f"~({unit})"))
+    space_unit = f" {unit}"
+
+    new_kb = []
+    for clause in kb:
+        # if we look for the unit 'p' in the sentence 'r | ~p' it is found
+        # therefore we will look for the string ' p', to ensure it has no negation
+        if clause == unit or space_unit in clause:
+            continue
+
+        if neg_unit in clause:
+            if '|' not in clause:  # unit clause of neg_unit implies a contradiction in kb
+                return False
+
+            # remove the negated unit from clauses
+            if f"{neg_unit} | " in clause:
+                new_kb.append(clause.replace(f"{neg_unit} | ", ""))
+            else:
+                new_kb.append(clause.replace(f" | {neg_unit}", ""))
+
+        else:
+            new_kb.append(clause)
+    return new_kb
+
+
+def sentence_to_clauses(sentence: str) -> List[str]:
+    try:
+        cnf = to_cnf(sentence)
+        clauses = str(cnf).split('&')
+
+    except SyntaxError:
+        raise Exception('Formula provided has wrong format.')
+
+    for i, clause in enumerate(clauses):
+        clauses[i] = clause.replace('(', '').replace(')', '').strip()
+    #TODO: Replace with map function
+    return clauses
